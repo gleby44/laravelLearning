f8520ad0fa95cf187d550efcefd57c4510ffdf8b
группы стационара <st@itstep.lan>
2017-11-28 19:37:32 +0300

    first message

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base') 
+@section('content')
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    My index
+                </div>
+            </div>
+        </div>
+@endsection
