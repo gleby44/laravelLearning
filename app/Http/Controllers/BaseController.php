602051441db341f3804f1eeecb1eb878a0245c49
группы стационара <st@itstep.lan>
2017-11-28 21:03:30 +0300

    dsf

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Request;
+
+use App\Maintext;
 
 class BaseController extends Controller
 {
 	public function getIndex(){
-		return view('index');
+		$obj=Maintext::where('url','index')->first();
+		//dd($obj);
+		return view('index',compact('obj'));
 	}
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,9 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    My index
+				{{$obj->name}}
                 </div>
+				{!!$obj->body!!}
             </div>
         </div>
 @endsection
